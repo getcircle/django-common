5406c22a3fa6d8a78866b01c89ae72fd9edf46c6
Michael Hahn <mwhahn@gmail.com>
2014-12-10 09:43:08 -0800

    Add UUIDModel

--- common/db/models.py
+++ common/db/models.py
@@ -1,6 +1,12 @@
+import uuid
 from django.db.models import *
 
 from django.db import models as django_models
+
+
+class UUIDModel(django_models.Model):
+
+    id = django_models.UUIDField(primary_key=True, default=uuid.uuid4)
 
 
 class TimestampableModel(django_models.Model):
