7cbd8155418b344dd4285942c3f5cca6c422a6e7
Michael Hahn <mwhahn@gmail.com>
2014-12-09 19:52:09 -0800

    fix NameError

--- common/db/models.py
+++ common/db/models.py
@@ -1,7 +1,9 @@
 from django.db.models import *
 
+from django.db import models as django_models
 
-class TimestampableModel(Model):
 
-    created = models.DateTimeField(auto_now_add=True)
-    changed = models.DateTimeField(auto_now=True)
+class TimestampableModel(django_models.Model):
+
+    created = django_models.DateTimeField(auto_now_add=True)
+    changed = django_models.DateTimeField(auto_now=True)
