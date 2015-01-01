800ffb98156b16b3157ed5e739e3b9b94643c3ba
Michael Hahn <mwhahn@gmail.com>
2014-12-31 15:18:10 -1000

    When calling `from_protobuf` make sure we run the field's `to_python` method on the value
    We serialize the model fields by calling `value_to_string`. If we
    roundtrip those objects, we need to call `to_python` when saving.

--- common/db/manager.py
+++ common/db/manager.py
@@ -2,6 +2,10 @@ from django.db import models as django_models
 from protobuf_to_dict import protobuf_to_dict
 
 
+class Null(object):
+    pass
+
+
 class CommonManager(django_models.Manager):
 
     def get_or_none(self, *args, **kwargs):
@@ -11,9 +15,16 @@ class CommonManager(django_models.Manager):
             return None
 
     def from_protobuf(self, protobuf, commit=True, **extra):
-        params = protobuf_to_dict(protobuf)
-        params.update(extra)
+        values = protobuf_to_dict(protobuf)
+        values.update(extra)
+
+        parameters = {}
+        for field in self.model._meta.fields:
+            value = values.get(field.attname, Null())
+            if not isinstance(value, Null):
+                parameters[field.attname] = field.to_python(value)
+
         if commit:
-            return self.create(**params)
+            return self.create(**parameters)
         else:
-            return self.model(**params)
+            return self.model(**parameters)
