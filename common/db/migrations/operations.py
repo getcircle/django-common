b0b01e17f24c5209fcab0af418e0c7ac46c46fff
Michael Hahn <mwhahn@gmail.com>
2015-02-06 14:49:56 -0800

    we need more information to create the rule

--- common/db/migrations/operations.py
+++ common/db/migrations/operations.py
@@ -25,18 +25,28 @@ class AddRule(Operation):
 
     reversible = True
 
-    def __init__(self, name, rule):
+    def __init__(self, name, table, event, rule):
         self.name = name
+        self.table = table
+        self.event = event
         self.rule = rule
 
     def state_forwards(self, *args, **kwargs):
         pass
 
+    def get_rule(self):
+        return 'CREATE RULE "%s" AS ON %s TO "%s" %s' % (
+            self.name,
+            self.event,
+            self.table,
+            self.rule,
+        )
+
     def database_forwards(self, app_label, schema_editor, from_state, to_state):
-        schema_editor.execute('CREATE RULE "%s" AS ON %s' % (self.name, self.rule,))
+        schema_editor.execute(self.get_rule())
 
     def database_backwards(self, app_label, schema_editor, from_state, to_state):
-        schema_editor.execute('DROP RULE "%s"' % (self.name,))
+        schema_editor.execute('DROP RULE "%s" ON "%s"' % (self.name, self.table))
 
     def describe(self):
-        return "Creates rule: %s - %s" % (self.name, self.rule)
+        return "Creates rule: %s" % (self.get_rule(),)
