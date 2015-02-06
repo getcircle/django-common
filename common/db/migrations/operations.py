01995794867cc7adb541902665fe4e51061c6075
Michael Hahn <mwhahn@gmail.com>
2015-02-06 14:44:56 -0800

    Add an AddRule migration operation

--- common/db/migrations/operations.py
+++ common/db/migrations/operations.py
@@ -19,3 +19,24 @@ class LoadExtension(Operation):
 
     def describe(self):
         return "Creates extension %s" % self.name
+
+
+class AddRule(Operation):
+
+    reversible = True
+
+    def __init__(self, name, rule):
+        self.name = name
+        self.rule = rule
+
+    def state_forwards(self, *args, **kwargs):
+        pass
+
+    def database_forwards(self, app_label, schema_editor, from_state, to_state):
+        schema_editor.execute('CREATE RULE "%s" AS ON %s' % (self.name, self.rule,))
+
+    def database_backwards(self, app_label, schema_editor, from_state, to_state):
+        schema_editor.execute('DROP RULE "%s"' % (self.name,))
+
+    def describe(self):
+        return "Creates rule: %s - %s" % (self.name, self.rule)
