59f8e062b7ed1d5a058b7e553603f4ef9955467b
Michael Hahn <mwhahn@gmail.com>
2015-03-24 15:20:48 -0700

    Reference the correct class for super

--- common/db/models/query.py
+++ common/db/models/query.py
@@ -14,40 +14,40 @@ class TimedQuerySet(QuerySet):
 
     def iterator(self, *args, **kwargs):
         with metrics.time(self._get_metric_name('iterator')):
-            results = super(QuerySet, self).iterator(*args, **kwargs)
+            results = super(TimedQuerySet, self).iterator(*args, **kwargs)
         return results
 
     def aggregate(self, *args, **kwargs):
         with metrics.time(self._get_metric_name('aggregate')):
-            results = super(QuerySet, self).aggregate(*args, **kwargs)
+            results = super(TimedQuerySet, self).aggregate(*args, **kwargs)
         return results
 
     def count(self, *args, **kwargs):
         with metrics.time(self._get_metric_name('count')):
-            results = super(QuerySet, self).count(*args, **kwargs)
+            results = super(TimedQuerySet, self).count(*args, **kwargs)
         return count
 
     def get(self, *args, **kwargs):
         with metrics.time(self._get_metric_name('get')):
-            results = super(QuerySet, self).get(*args, **kwargs)
+            results = super(TimedQuerySet, self).get(*args, **kwargs)
         return results
 
     def create(self, *args, **kwargs):
         with metrics.time(self._get_metric_name('create')):
-            results = super(QuerySet, self).create(*args, **kwargs)
+            results = super(TimedQuerySet, self).create(*args, **kwargs)
         return results
 
     def bulk_create(self, *args, **kwargs):
         with metrics.time(self._get_metric_name('bulk_create')):
-            results = super(QuerySet, self).bulk_create(*args, **kwargs)
+            results = super(TimedQuerySet, self).bulk_create(*args, **kwargs)
         return results
 
     def update(self, *args, **kwargs):
         with metrics.time(self._get_metric_name('update')):
-            results = super(QuerySet, self).update(*args, **kwargs)
+            results = super(TimedQuerySet, self).update(*args, **kwargs)
         return results
 
     def exists(self, *args, **kwargs):
         with metrics.time(self._get_metric_name('exists')):
-            results = super(QuerySet, self).exists(*args, **kwargs)
+            results = super(TimedQuerySet, self).exists(*args, **kwargs)
         return results
