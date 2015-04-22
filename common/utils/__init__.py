f8028ce653fd13615b83502b0201e5c54e45e2e6
Michael Hahn <mwhahn@gmail.com>
2015-04-22 12:36:57 -0700

    Add helper to get django model choices from protobuf_enum

--- common/utils/__init__.py
+++ common/utils/__init__.py
@@ -6,3 +6,8 @@ def uuid_from_hex(hex_value, version=4):
         return uuid.UUID(hex=hex_value, version=version)
     except ValueError:
         return None
+
+
+def model_choices_from_protobuf_enum(protobuf_enum):
+    """Protobufs Enum "items" is the opposite order djagno requires"""
+    return [(x[1], x[0]) for x in protobuf_enum.items()]
